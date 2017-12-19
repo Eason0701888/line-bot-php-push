f9f27eb675f7889b0027cd6bb1ed86baf106c337
SHanDesign1993 <SHanDesign@users.noreply.github.com>
2017-12-19 10:56:29 +0800

    c

--- testsend.php
+++ testsend.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $CHANNEL_ACCESS_TOKEN="VfcdUobfc4kKV+SbzTXQDOnvBjpFIkJqla35htny9vPB+3uVC9B5Dkwy6e6eSDGjbJQyHQMVk9+W9ALd7+Px4LcmAZMxoaE1Qyvgkzk0B99Q6vQCMusoUsjR6UHtZDHzgYrNY9OYzefblJvD7CxNCwdB04t89/1O/w1cDnyilFU=";
 $CHANNEL_SECRET="7e61c3a59c473a50f7d2f691669751a0";
 $to="Ue004c4797cf171301dccf7e7d8ef7d62";
+$to="henryleehaha";
 
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($CHANNEL_ACCESS_TOKEN);
@@ -29,7 +30,7 @@ $curl = curl_init() ;
 curl_setopt($curl, CURLOPT_URL, "https://api.line.me/v2/bot/message/push") ;
 curl_setopt($curl, CURLOPT_HEADER, true);
 curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . $CHANNEL_ACCESS_TOKEN));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json;charset=UTF-8 ", "Authorization: Bearer " . $CHANNEL_ACCESS_TOKEN));
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($message_obj));
 $result=curl_exec($curl);
