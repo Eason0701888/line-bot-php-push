6a4e164c6f937b2a35e047cbb4c3e095fbeac80a
SHanDesign1993 <SHanDesign@users.noreply.github.com>
2017-12-19 10:58:58 +0800

    c

--- testsend.php
+++ testsend.php
@@ -4,8 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 $CHANNEL_ACCESS_TOKEN="VfcdUobfc4kKV+SbzTXQDOnvBjpFIkJqla35htny9vPB+3uVC9B5Dkwy6e6eSDGjbJQyHQMVk9+W9ALd7+Px4LcmAZMxoaE1Qyvgkzk0B99Q6vQCMusoUsjR6UHtZDHzgYrNY9OYzefblJvD7CxNCwdB04t89/1O/w1cDnyilFU=";
 $CHANNEL_SECRET="7e61c3a59c473a50f7d2f691669751a0";
 $to="Ue004c4797cf171301dccf7e7d8ef7d62";
-$to="henryleehaha";
-
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($CHANNEL_ACCESS_TOKEN);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $CHANNEL_SECRET]);
